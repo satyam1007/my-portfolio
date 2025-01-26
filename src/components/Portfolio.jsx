b18e2a2413811b487ab659bf0206d3be68f00abe
Satyam Mishra <142882703+satyam1007@users.noreply.github.com>
2025-01-26 13:57:22 +0530

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,6 +6,7 @@ import { FaBarsStaggered } from "react-icons/fa6";
 import { FaTimes } from "react-icons/fa";
 import { FaGithubSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
 import { HiMiniViewfinderCircle } from "react-icons/hi2";
 
 function Portfolio() {
@@ -156,6 +157,12 @@ function Portfolio() {
                   className="text-3xl dark:text-gray-400 text-gray-700 hover:text-black hover:scale-110 duration-200"
                 >
                   <FaGithubSquare />
+                </a>
+                <a
+                  href="https://www.instagram.com/satyam07.dev/"
+                  className="text-3xl bg-gradient-to-r from-[#833ab4] via-[#fd1d1d] to-[#fcb045] hover:scale-110 duration-200"
+                >
+                  <FaSquareInstagram />
                 </a>
                 <a
                   href="#"
